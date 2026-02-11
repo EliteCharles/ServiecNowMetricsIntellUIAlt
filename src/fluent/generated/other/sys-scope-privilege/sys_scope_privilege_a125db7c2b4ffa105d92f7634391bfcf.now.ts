b9dfe1373cfc858edb4ef2688ea8f71f2778206c
System Administrator <admin@example.com>
2026-02-11 13:40:02 -0600

    scope changes.

---
+++ src/fluent/generated/other/sys-scope-privilege/sys_scope_privilege_a125db7c2b4ffa105d92f7634391bfcf.now.ts
@@ -0,0 +1,10 @@
+import { CrossScopePrivilege } from '@servicenow/sdk/core'
+
+CrossScopePrivilege({
+    $id: Now.ID['a125db7c2b4ffa105d92f7634391bfcf'],
+    operation: 'read',
+    status: 'allowed',
+    targetName: 'em_alert_anomaly',
+    targetScope: 'global',
+    targetType: 'sys_db_object',
+})
---
+++ src/fluent/generated/other/sys-scope-privilege/sys_scope_privilege_a925db7c2b4ffa105d92f7634391bf7c.now.ts
@@ -0,0 +1,10 @@
+import { CrossScopePrivilege } from '@servicenow/sdk/core'
+
+CrossScopePrivilege({
+    $id: Now.ID['a925db7c2b4ffa105d92f7634391bf7c'],
+    operation: 'read',
+    status: 'allowed',
+    targetName: 'em_alert',
+    targetScope: 'global',
+    targetType: 'sys_db_object',
+})
